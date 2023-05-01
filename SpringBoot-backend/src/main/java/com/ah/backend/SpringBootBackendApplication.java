7c7a193a0d6d59b8ccbef6bb6a78668aeec6112f
LiuHao <liuhaoai545@gmail>
2023-05-01 21:14:06 +0800

    init

---
+++ SpringBoot-backend/src/main/java/com/ah/backend/SpringBootBackendApplication.java
@@ -0,0 +1,17 @@
+package com.ah.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBootBackendApplication
+ * @author ahao
+ */
+@SpringBootApplication
+public class SpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendApplication.class, args);
+	}
+
+}
